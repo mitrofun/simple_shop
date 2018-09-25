9895130a9117497b550e7e4173343c2064585189
Dmitriy Shesterkin <mitri4@bk.ru>
2018-09-25 10:39:15 +0300

    add copy fixtures media files for quick develop

--- quick_start.sh
+++ quick_start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 echo 'Run script: quick start for local development'
 make migrate
+mkdir -p media/products && cp -rn fixtures/products media
 make loaddata
 make user
 make run
